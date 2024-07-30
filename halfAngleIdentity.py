a2eb668b11b2ee7cdf0eaac796befd4b703c61a0
Adam Marton Beke <65732867+BekeAtGithub@users.noreply.github.com>
2024-07-29 19:03:12 -0700

    Update halfAngleIdentity.py

--- halfAngleIdentity.py
+++ halfAngleIdentity.py
@@ -2,7 +2,7 @@
 The half-angle identities in trigonometry are used to express trigonometric functions of half-angles in terms of the original angle. Here are the half-angle identities:
 
     sin⁡(θ2)=±1−cos⁡(θ)2sin(2θ​)=±21−cos(θ)​
-
+θ = theta sign, not a zero
 ​
 cos⁡(θ2)=±1+cos⁡(θ)2cos(2θ​)=±21+cos(θ)​
 ​
